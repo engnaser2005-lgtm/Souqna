b25a06dddd930be9aa048cd4373c1a4c5ac5945a
engnaser2005-lgtm <engnaser2005@gmail.com>
2026-05-13 06:53:53 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const supabaseUrl = 'https://dkhjaktcjbnndxtxcetf.supabase.co'
+
+const supabaseKey = 'ضع_هنا_مفتاح_publishable_key'
+
+const supabase = window.supabase.createClient(
+supabaseUrl,
+supabaseKey
+)
